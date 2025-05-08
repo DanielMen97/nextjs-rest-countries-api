66c5611eb931700e5caee0809d6ece9ce009c47b
Cristian Daniel <cristian.mendozapds@gmail.com>
2025-05-07 21:41:17 -0500

    Feature Next JS Countries Rest Api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,54 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
+import loupe from "../../public/loupe.svg";
+import moon from "../../public/moon.svg";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
+    <>
+      <header className={`${styles.header} center-between`}>
+        <h1 className={styles.title}>Where in the world?</h1>
+        <button className={`${styles.toogleDarkMode} center`}>
+          <Image src={moon} alt="moon icon" width={20} height={20} />
+          Dark Mode
+        </button>
+      </header>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <form className={`${styles.form} center-between`}>
+          <label htmlFor="search" className={styles.label}>
             <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
+              src={loupe}
+              alt="search icon"
               width={20}
               height={20}
+              className={styles.searchIcon}
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
+          <input
+            type="text"
+            className={styles.input}
+            placeholder="Search for a country..."
+          />
+          </label>
+          <select className={styles.select}>
+            <option value="africa">Africa</option>
+          </select>
+        </form>
+        <section className={styles.countries}>
+          <article className={styles.country}>
+            <Image
+              src="https://flagcdn.com/af.svg"
+              alt="country flag"
+              width={300}
+              height={200}
+            />
+            <h2 className={styles.countryName}>Afghanistan</h2>
+            <p className={styles.countryInfo}>Population: 38,928,346</p>
+            <p className={styles.countryInfo}>Region: Asia</p>
+            <p className={styles.countryInfo}>Capital: Kabul</p>
+          </article>
+          {/* Add more country cards here */}
+        </section>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
